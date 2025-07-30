3d3ce09fa83d07e5591d76c0ba17268f74ed6334
Artem Kirienko <artkirienko@icloud.com>
2025-07-30 01:25:58 -0700

    weird redesign :zap:

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,35 +1,34 @@
 class ForecastsController < ApplicationController
   def show
-    @address = params[:address]
+    @address = params[:address]&.strip
+    @error = nil
 
-    if @address.blank? && params.key?(:button)
-      flash.now[:notice] = "Please enter an address to get the forecast."
-      # If you render a form here, wrap it in the same Turbo Frame ID.
-      # For now, we'll just show a message inside the frame.
-    elsif @address.blank?
-      # do nothing, just render the form
-    elsif !valid_address_format?(@address)
-      flash.now[:alert] = "The provided address has an invalid format. Please try again."
-    else
-      begin
-        # Simulate fetching forecast data
-        Rails.logger.info "Simulating 2-second delay before fetching forecast for #{@address}..."
-        sleep 2
-        @forecast_data = {
-          location: @address,
-          temperature: rand(10..30), # Random temp between 10 and 30
-          conditions: [ "Sunny", "Cloudy", "Rainy", "Partly Cloudy" ].sample,
-          wind_speed: rand(5..20)
-        }
-      rescue StandardError => e
-        flash.now[:alert] = "Could not fetch forecast for '#{@address}': #{e.message}"
-      end
+    return if @address.blank? && !params.key?(:commit)
+
+    if @address.blank?
+      @error = "Please enter an address to get the forecast."
+      return
+    end
+
+    unless valid_address_format?(@address)
+      @error = "The provided address has an invalid format. Please use letters, numbers, spaces, commas, periods, or hyphens."
+      return
+    end
+
+    begin
+      @forecast_data = WeatherService.new(@address).fetch_forecast
+    rescue WeatherService::NoForecastError
+      @error = "No forecast found for \"#{@address}\". Please try a different address, city, or zip code."
+    rescue StandardError => e
+      @error = "Could not fetch forecast for \"#{@address}\": #{e.message}"
     end
   end
 
   private
 
   def valid_address_format?(address)
-    address.is_a?(String) && address.length.between?(3, 100) && address.match?(/\A[a-zA-Z0-9\s,.\-]+\z/)
+    return false unless address.is_a?(String)
+    return false unless address.length.between?(3, 100)
+    address.match?(/\A[a-zA-Z0-9\s,.\-]+\z/) && !address.match?(/[!@#$%^&*()_=+{}\[\]|\\:;"'<>?\/~`]+/)
   end
 end
